8485fbd2478b9fe11c2bc6f646b2b0b1d7749709
now <lynx.ky@outlook.com>
2025-06-26 17:22:14 -0300

    Update LuauBundler.cpp

--- src/LuauBundler.cpp
+++ src/LuauBundler.cpp
@@ -13,27 +13,22 @@ std::string LuauBundler::generate() const {
   std::stringstream ss;
 
   ss << "local __modules = {}\n"
-        "function require(module)\n"
-        "  if type(module) == 'string' then\n"
-        "     local f = __modules[module]\n"
-        "     if not f then error(\"module '\"..module..\"' not found\") end\n"
-        "     return f()"
-        "  else\n"
-        "     return require(module)"
-        "  end\n"
+        "local oldRequire = require\n\n"
+        "function require(mod)\n"
+        "  if type(mod) == 'function' then return mod() end\n"
+        "  if type(mod) ~= 'string' then return oldRequire(mod) end\n"
+        "  local src = __modules[mod]\n"
+        "  if not src then error(\"module '\"..mod..\"' not found\", 2) end\n"
+        "  local fn, err = loadstring(src, '@'..mod)\n"
+        "  if not fn then error(err, 2) end\n"
+        "  return fn()\n"
         "end\n\n";
 
   for (const auto &[key, content] : modules) {
-    ss << "__modules[\"" << key << "\"] = function()\n";
-
-    std::istringstream in(content);
-    std::string line;
-    while (std::getline(in, line))
-      ss << "  " << line << "\n";
-
-    ss << "end\n\n";
+    ss << "__modules[\"" << key << "\"] = ";
+    ss << "[=[" << content << "]=]\n";
   }
 
-  ss << "require(\"" << entryModule << "\")\n";
+  ss << "\nrequire(\"" << entryModule << "\")\n";
   return ss.str();
 }
